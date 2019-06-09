a82a8d65ae8b00445a22ce6dc442c85979039675
Seth <18116602+sethkor@users.noreply.github.com>
2019-06-09 19:23:53 +1000

    Fix bug with bucket location.  Will thry to get the bucket location first before falling back to head object

--- common.go
+++ common.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/aws/credentials"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"go.uber.org/zap"
@@ -18,40 +17,63 @@ func checkBucket(sess *session.Session, bucket string) (svc *s3.S3, err error) {
 
 	var logger = zap.S()
 
-	svc = s3.New(session.Must(session.NewSession(&aws.Config{
-		Credentials: credentials.AnonymousCredentials,
-		Region:      sess.Config.Region,
-	})))
+	svc = s3.New(sess)
 
-	req, _ := svc.HeadBucketRequest(&s3.HeadBucketInput{
+	result, err := svc.GetBucketLocation(&s3.GetBucketLocationInput{
 		Bucket: aws.String(bucket),
 	})
 
-	err = req.Send()
+	if err == nil {
+		svc = s3.New(sess, &aws.Config{MaxRetries: aws.Int(30),
+			Region: aws.String(s3.NormalizeBucketLocation(*result.LocationConstraint))})
 
-	if err != nil {
-		fmt.Println(err)
+		return
+	} else {
 
 		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
 
-			case s3.ErrCodeNoSuchBucket:
+			if aerr.Code() == s3.ErrCodeNoSuchBucket {
+				fmt.Println(aerr.Message())
 				logger.Fatal(aerr.Message())
-			default:
-				logger.Fatal(aerr.Error())
-
+				return
 			}
-		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
-			logger.Fatal(err.Error())
-		}
-		return svc, err
-	} //if
 
-	req.HTTPResponse.Header.Get("X-Amz-Bucket-Region")
-	svc = s3.New(sess, &aws.Config{MaxRetries: aws.Int(30),
-		Region: aws.String(s3.NormalizeBucketLocation(req.HTTPResponse.Header.Get("X-Amz-Bucket-Region")))})
+			//Try getting the region via head-object.
+			svc = s3.New(session.Must(session.NewSession(&aws.Config{
+				Credentials: credentials.AnonymousCredentials,
+				Region:      sess.Config.Region,
+			})))
+
+			req, _ := svc.HeadBucketRequest(&s3.HeadBucketInput{
+				Bucket: aws.String(bucket),
+			})
+
+			err = req.Send()
+			if err != nil {
+				fmt.Println(err)
+
+				if aerr, ok := err.(awserr.Error); ok {
+					switch aerr.Code() {
+
+					case s3.ErrCodeNoSuchBucket:
+						logger.Fatal(aerr.Message())
+					default:
+						logger.Fatal(aerr.Error())
+
+					}
+				} else {
+					// Print the error, cast err to awserr.Error to get the Code and
+					// Message from an error.
+					logger.Fatal(err.Error())
+				}
+				return svc, err
+			} //if
+
+			req.HTTPResponse.Header.Get("X-Amz-Bucket-Region")
+			svc = s3.New(sess, &aws.Config{MaxRetries: aws.Int(30),
+				Region: aws.String(s3.NormalizeBucketLocation(req.HTTPResponse.Header.Get("X-Amz-Bucket-Region")))})
+		}
+	}
 
 	return svc, err
 }
